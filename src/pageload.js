4c5257225d8f6dab97126a7119eee12ce1a2d48c
Dean Pooler <Deanpooler@outlook.com>
2020-11-20 18:56:31 +0100

    Add configureStyle and move style to css file

--- src/pageload.js
+++ src/pageload.js
@@ -2,45 +2,43 @@ import setText from './setText';
 const content = document.getElementById('content');
 
 const pageload = () => {
-    createBanner();
+    configureStyle();
     createHeader();
     createHorizontalRule();
     createParagraph('/dist/lang/homeparagraph.txt');
 }
 
-const createBanner = () => {
-    const banner = document.createElement('img');
-    banner.style.display = 'block';
-    banner.style.marginLeft = 'auto';
-    banner.style.marginRight = 'auto';
-    banner.style.width = '60%';
-    banner.setAttribute('src', '/dist/img/food-dishes-header.jpg');
-    content.appendChild(banner);
-}
-
 const createHeader = () => {
+    const container = document.createElement('div');
     const header = document.createElement('h1');
-    header.textContent = 'Soma Kitchens';
-    header.style.textAlign = 'center';
-    content.appendChild(header);
+    container.setAttribute('id', 'header');
+    header.textContent = 'Soma\'s Kitchen';
+    container.appendChild(header);
+    content.appendChild(container);
 }
 
 const createHorizontalRule = () => {
     const hr = document.createElement('hr');
-    hr.style.border = '0';
-    hr.style.width = '60%'
-    hr.style.height = '1px';
-    hr.style.background = '#333';
-    hr.style.backgroundImage = 'linear-gradient(to right, #ccc, #333, #ccc';
+    hr.setAttribute('class', 'hr');
     content.appendChild(hr);
 }
 
 const createParagraph = (file) => {
     const p = document.createElement('p');
-    p.style.textAlign = 'center';
-    p.textContent = '';
+    p.setAttribute('class', 'text-content')
     setText(file, p);
     content.appendChild(p);
 }
 
+const configureStyle = () => {
+    var link = document.createElement('link');
+    var stylesheet = document.createElement('link');
+    stylesheet.setAttribute('rel', 'stylesheet');
+    stylesheet.setAttribute('href', '/dist/style/style.css')
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('type', 'text/css')
+    link.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400&display=swap');
+    document.head.appendChild(link);
+    document.head.appendChild(stylesheet);
+}
 export default pageload;
