64e9378bbeae842564865929249b70592d153f2a
Dean Pooler <Deanpooler@outlook.com>
2020-11-21 02:27:36 +0100

    Add menu button handlers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import pageload from './pageload';
+import { pageload, addContent } from './pageload';
+import contact from './contact';
 
 pageload();
+
+const navButtons = document.querySelectorAll('.nav-button');
+navButtons.forEach((button) => {
+  if (button.id == 'home-button') { button.addEventListener('click', addContent) }
+  if (button.id == 'contact-button') { button.addEventListener('click', contact); }
+  button.addEventListener('click', () => {
+    document.getElementById('content').remove()
+  });
+});
+
+
+const homeButton = document.getElementById('home-button');
+const menuButton = document.getElementById('menu-button');
+
+const contactButton = document.getElementById('contact-button')
